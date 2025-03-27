4e3f3186fc420c32ec00f40fdc5dfe9afb182283
MarwanAlsabbagh <161840298+MarwanAlsabbagh@users.noreply.github.com>
2025-03-27 07:12:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def down(url):
                 filename=str(str(filename[0:80])+".txt")
 
             file_path = os.path.join(download_dir, filename)
-            if len(response.iter_content<=7000:
+            if len(response.iter_content)<=7000:
                 with open(file_path, 'wb') as f:
                     for chunk in response.iter_content(chunk_size=100):#10000
                         if chunk:
