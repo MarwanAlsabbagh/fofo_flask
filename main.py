6ed768b04e74baf6104546e1faab4d3d53c6d4e6
MarwanAlsabbagh <161840298+MarwanAlsabbagh@users.noreply.github.com>
2025-04-28 15:19:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def chatbot1():
         # المادة 12 من الدستور المصري تنص على أن التعليم حق لكل مواطن، هدفه بناء الشخصية المصرية، الحفاظ على الهوية الوطنية، وتأكيد قيم المنهج العلمي، وتنمية المواهب، وتشجيع الابتكار
         
         API_URL = "https://api-inference.huggingface.co/models/deepset/xlm-roberta-large-squad2"
-        headers = {"Authorization": "Bearer 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"}
+        headers = {"Authorization": "Bearer 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"}
         
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
